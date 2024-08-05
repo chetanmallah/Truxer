8a3bb3271b837ead7d1ab269bdc44380610e4ccf
chetan mallah <chetanmallah28@gmail.com>
2024-08-06 00:52:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop";  // Import the ScrollToTop component
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import About from "./Pages/About/About";
+import Service from "./components/Services/Services";
+
+import ContactUs from "./Pages/ContactUs/ContactUs";
+import Driver_Hirng from "./Pages/Driver_Hiring/Driver_Hiring";
+import Booking from "./Pages/Booking/Booking";
+import WhyChooseUsPage from "./Pages/WhyChooseUsPage/WhyChooseUsPage";
+
+function App() {
+    return (
+        <Router>
+            <Navbar />
+            <ScrollToTop />  {/* Add ScrollToTop here */}
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/vehicles" element={<Service />} />
+                <Route path="/booking" element={<Booking />} />
+                <Route path="/contactus" element={<ContactUs />} />
+                <Route path="/driverhiring" element={<Driver_Hirng />} />
+                <Route path="/faq" element={<WhyChooseUsPage />} />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
